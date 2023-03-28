e2ffa583224ff68da916613f308b1efc930e3123
Elio Muculo <eliomuculo@MACBOOKs-MacBook-Pro-3.local>
2023-03-29 00:32:59 +0200

    add controller

--- src/main/java/com/goldthings/loja/boot/GoldthingsApplication.java
+++ src/main/java/com/goldthings/loja/boot/GoldthingsApplication.java
@@ -2,12 +2,23 @@ package com.goldthings.loja.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GoldthingsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GoldthingsApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello world, f**ick Working";
 	}
 
 }
